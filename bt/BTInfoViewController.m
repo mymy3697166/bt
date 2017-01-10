a4dcb0b44784dad0c214cd20258ddb3c846d3b7b
mymy3697166 <644480808@qq.com>
2017-01-10 23:52:29 +0800

    asdf

--- bt/BTInfoViewController.m
+++ bt/BTInfoViewController.m
@@ -9,19 +9,76 @@
 #import "BTInfoViewController.h"
 
 @interface BTInfoViewController () {
+  __weak IBOutlet UIView *avatarBgView;
   
   __weak IBOutlet UIView *genderBgFView;
   __weak IBOutlet UIView *genderFView;
+  __weak IBOutlet UIButton *btnGenderF;
+  
+  __weak IBOutlet UIView *genderBgMView;
+  __weak IBOutlet UIView *genderMView;
+  __weak IBOutlet UIButton *btnGenderM;
+  
+  NSString *gender;
 }
-
 @end
 
 @implementation BTInfoViewController
 - (void)viewDidLoad {
   [super viewDidLoad];
-  genderBgFView.clipsToBounds = YES;
-  genderBgFView.layer.cornerRadius = 20;
-  genderFView.clipsToBounds = YES;
-  genderFView.layer.cornerRadius = 19;
+  [self initUI];
+  gender = @"F";
 }
+
+- (void)initUI {
+  avatarBgView.clipsToBounds = YES;
+  avatarBgView.layer.cornerRadius = avatarBgView.bounds.size.width / 2;
+  
+  genderBgFView.clipsToBounds = YES;
+  genderBgFView.layer.cornerRadius = 16;
+  genderFView.clipsToBounds = YES;
+  genderFView.layer.cornerRadius = 15;
+  
+  genderBgMView.clipsToBounds = YES;
+  genderBgMView.layer.cornerRadius = 16;
+  genderMView.clipsToBounds = YES;
+  genderMView.layer.cornerRadius = 15;
+}
+
+- (IBAction)genderFClick:(UIButton *)sender {
+  btnGenderF.titleLabel.textColor = RGB(236, 82, 72);
+  [btnGenderF setTitleColor:RGB(236, 82, 72) forState:UIControlStateNormal];
+  genderBgFView.backgroundColor = RGB(236, 82, 72);
+  
+  btnGenderM.titleLabel.textColor = [UIColor lightGrayColor];
+  [btnGenderM setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+  genderBgMView.backgroundColor = [UIColor lightGrayColor];
+  
+  gender = @"F";
+}
+
+- (IBAction)genderMClick:(UIButton *)sender {
+  btnGenderM.titleLabel.textColor = RGB(0, 178, 255);
+  [btnGenderM setTitleColor:RGB(0, 178, 255) forState:UIControlStateNormal];
+  genderBgMView.backgroundColor = RGB(0, 178, 255);
+  
+  btnGenderF.titleLabel.textColor = [UIColor lightGrayColor];
+  [btnGenderF setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+  genderBgFView.backgroundColor = [UIColor lightGrayColor];
+  
+  gender = @"M";
+}
+
+- (IBAction)dobClick:(UITapGestureRecognizer *)sender {
+  NSLog(@"asdf");
+}
+
+- (IBAction)nextClick:(id)sender {
+  
+}
+
+- (IBAction)backClick:(UIBarButtonItem *)sender {
+  [self.navigationController popViewControllerAnimated:YES];
+}
+
 @end
